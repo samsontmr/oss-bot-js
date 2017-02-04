6791d31957b79a3ff69eac69d5cf734ea935d86b
Samson <samsontmr@users.noreply.github.com>
2017-02-04 17:55:22 +0800

    Add code to comment on PR if conventions are violated

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var GitHub = require('github-api');
+
+// basic auth
+var gh = new GitHub({
+   token: process.env.GITHUB_API_TOKEN
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -20,8 +26,12 @@ function receive_pull_request(request, response) {
     console.log("Received pull request: \n" + request.body);
     response.send();
     extractedPrDetails = extractRelevantDetails(request);
-    if (validatePullRequest(extractedPrDetails)) {
-        console.log("Check passed!");
+    if (!validatePullRequest(extractedPrDetails)) {
+        gh.createIssueComment(extractedPrDetails.id,
+                              'Hi ' + extractedPrDetails.username +
+                              ', please follow the naming conventions for PRs.')
+        console.log("Check Failed!");
+
     }
 }
 
@@ -29,9 +39,10 @@ function extractRelevantDetails(received_json) {
     title = received_json.body.pull_request.title;
     body = received_json.body.pull_request.body;
     username = received_json.body.pull_request.user.login;
-    console.log('Received PR "' + title + '" from: ' + username +
+    id = received_json.body.pull_request.number;
+    console.log('Received PR ' + id + ' "' + title + '" from: ' + username +
                 '\n Description: "' + body + '"');
-    return {title : title, body : body, username : username};
+    return {id : id, title : title, body : body, username : username};
 }
 
 function validatePullRequest(prDetails) {
@@ -40,12 +51,12 @@ function validatePullRequest(prDetails) {
 
 function validatePullRequestTitle(prTitle) {
     titleTest = new RegExp(process.env.REGEX_PULL_REQ_TITLE);
-    console.log("Regex for title: " + process.env.REGEX_PULL_REQ_TITLE)
+    console.log("Regex for title: " + process.env.REGEX_PULL_REQ_TITLE);
     return titleTest.test(prTitle);
 }
 
 function validatePullRequestBody(prBody) {
-    titleTest = new RegExp(process.env.REGEX_PULL_REQ_BODY);
-    console.log("Regex for body: " + process.env.REGEX_PULL_REQ_BODY)
-    return titleTest.test(prBody);
+    bodyTest = new RegExp(process.env.REGEX_PULL_REQ_BODY);
+    console.log("Regex for body: " + process.env.REGEX_PULL_REQ_BODY);
+    return bodyTest.test(prBody);
 }
