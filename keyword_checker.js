b0c58e45520a8218b12008ef314dfbfc79a8f234
Samson <samsontmr@users.noreply.github.com>
2017-02-28 11:34:53 +0800

    Add method comments for check

--- keyword_checker.js
+++ keyword_checker.js
@@ -1,4 +1,9 @@
 module.exports = {
+    /*
+    * Scans input string for convention violations and returns feedback based on
+    * detected violations
+    * Returns a string containing the formatted feedback message
+    */
     getFeedback: function (string) {
         message = "";
         
@@ -19,16 +24,28 @@ module.exports = {
     }
 };
 
+/*
+* Checks if an issue reference is present in argument
+* Returns true if present, false otherwise
+*/
 function detectIssueReference(string) {
     referenceTest = new RegExp(/[\s\S]*#\d[\s\S]*/);
     return referenceTest.test(string);
 }
 
+/*
+* Checks if argument contains a space between a # and a digit
+* Returns true if present, false otherwise
+*/
 function detectSpaceBetweenHashtagAndDigit(string) {
     spaceTest = new RegExp(/[\s\S]*# \d[\s\S]*/);
     return spaceTest.test(string);
 }
 
+/*
+* Detects if a GitHub keyword is present in argument
+* Returns true if present, false otherwise
+*/
 function detectGithubKeyword(string) {
     keywordTest = new RegExp(/[\s\S]*(([F|f]ix(e[d|s])?)|([C|c]lose[d|s]?)|([R|r]esolve[d|s]?))[\s\S]*/);
     return keywordTest.test(string);
