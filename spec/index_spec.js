2d42dd472df2b2f45a46fad08fda93b30d798391
Li Kai <falconets25601@gmail.com>
2017-03-09 14:04:44 +0800

    migrate spec test files to es6

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -1,50 +1,50 @@
-var index = require('../index.js');
+const index = require('../index.js');
 
-describe('index', function () {
-  describe('reponse message', function () {
-    beforeEach(function() {
+describe('index', () => {
+  describe('reponse message', () => {
+    beforeEach(() => {
       process.env.REGEX_PULL_REQ_TITLE = '\\S\\s';
       process.env.REGEX_PULL_REQ_BODY = '\\S\\s';
       process.env.ENABLE_KEYWORD_CHECKER = 'false';
       process.env.CONTRIBUTING_GUIDELINES = 'test';
     });
 
-    it('should produce response message header with link to guidelines, if regex passes', function () {
+    it('should produce response message header with link to guidelines, if regex passes', () => {
       process.env.REGEX_PULL_REQ_TITLE = '\\S\\s';
       process.env.REGEX_PULL_REQ_BODY = '\\S\\s';
       process.env.ENABLE_KEYWORD_CHECKER = 'false';
       process.env.CONTRIBUTING_GUIDELINES = 'test';
-      var pullRequest = {
+      const pullRequest = {
         username: 'JohnDoe',
         title: 'A title',
-        body: 'A body'
+        body: 'A body',
       };
-      var message = index.buildResponseMessage(pullRequest);
-      var expectedMessage = 'Hi @JohnDoe, these parts of your pull request do not appear to follow our [contributing guidelines](test):\n\n';
+      const message = index.buildResponseMessage(pullRequest);
+      const expectedMessage = 'Hi @JohnDoe, these parts of your pull request do not appear to follow our [contributing guidelines](test):\n\n';
       expect(message).toEqual(expectedMessage);
     });
 
-    it('should feedback when PR title does not match regex', function () {
+    it('should feedback when PR title does not match regex', () => {
       process.env.REGEX_PULL_REQ_TITLE = '#';
-      var pullRequest = {
+      const pullRequest = {
         username: 'JohnDoe',
         title: 'A title',
-        body: 'A body'
+        body: 'A body',
       };
-      var message = index.buildResponseMessage(pullRequest);
-      var expectedMessage = 'Hi @JohnDoe, these parts of your pull request do not appear to follow our [contributing guidelines](test):\n\n1. PR Title\n';
+      const message = index.buildResponseMessage(pullRequest);
+      const expectedMessage = 'Hi @JohnDoe, these parts of your pull request do not appear to follow our [contributing guidelines](test):\n\n1. PR Title\n';
       expect(message).toEqual(expectedMessage);
     });
 
-    it('should feedback when PR message does not match regex', function () {
+    it('should feedback when PR message does not match regex', () => {
       process.env.REGEX_PULL_REQ_BODY = '#';
-      var pullRequest = {
+      const pullRequest = {
         username: 'JohnDoe',
         title: 'A title',
-        body: 'A body'
+        body: 'A body',
       };
-      var message = index.buildResponseMessage(pullRequest);
-      var expectedMessage = 'Hi @JohnDoe, these parts of your pull request do not appear to follow our [contributing guidelines](test):\n\n1. PR Description\n';
+      const message = index.buildResponseMessage(pullRequest);
+      const expectedMessage = 'Hi @JohnDoe, these parts of your pull request do not appear to follow our [contributing guidelines](test):\n\n1. PR Description\n';
       expect(message).toEqual(expectedMessage);
     });
   });
