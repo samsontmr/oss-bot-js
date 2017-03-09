412cc839199c44a784076d8ebc7e4131675227ab
Samson <samsontmr@users.noreply.github.com>
2017-03-09 17:46:44 +0800

    Update keyword checker tests

--- spec/keyword_checker_spec.js
+++ spec/keyword_checker_spec.js
@@ -3,27 +3,29 @@ const keyword_checker = require('../keyword_checker.js');
 
 describe('keyword_checker', () => {
   it('should feedback when string contains space between # and digit', () => {
-    const feedback = keyword_checker.getFeedback('Fixed # 27');
-    const expectedMessage = '   * There should not be a space between the `#` and `issue-number`.\n';
-    expect(feedback).toEqual(expectedMessage);
+    const violations = keyword_checker.getDetailedViolations('Fixed # 27');
+    const expectedViolations = { spaceBetweenHashtagAndDigit: true };
+    expect(violations).toEqual(expectedViolations);
   });
 
   it('should feedback when string is missing issue reference', () => {
-    const feedback = keyword_checker.getFeedback('Fixed something');
-    const expectedMessage = '   * Issue Reference (`#<issue-number>`) missing.\n';
-    expect(feedback).toEqual(expectedMessage);
+    const violations = keyword_checker.getDetailedViolations('Fixed something');
+    const expectedViolations = { noIssueReference: true };
+    expect(violations).toEqual(expectedViolations);
   });
 
   it('should feedback when string does not contain GitHub keyword', () => {
-    const feedback = keyword_checker.getFeedback('Repaired #123');
-    const expectedMessage = '   * GitHub Keyword missing: Refer [here](https://help.github.com/articles/closing-issues-via-commit-messages/#keywords-for-closing-issues) for a list of accepted keywords.\n';
-    expect(feedback).toEqual(expectedMessage);
+    const violations = keyword_checker.getDetailedViolations('Repaired #123');
+    const expectedViolations = { missingGithubKeyword: true };
+    expect(violations).toEqual(expectedViolations);
   });
 
   it('should feedback both issue and keyword issues in string', () => {
-    const feedback = keyword_checker.getFeedback('Repaired # 1');
-    let expectedMessage = '   * There should not be a space between the `#` and `issue-number`.\n';
-    expectedMessage += '   * GitHub Keyword missing: Refer [here](https://help.github.com/articles/closing-issues-via-commit-messages/#keywords-for-closing-issues) for a list of accepted keywords.\n';
-    expect(feedback).toEqual(expectedMessage);
+    const violations = keyword_checker.getDetailedViolations('Repaired # 1');
+    const expectedViolations = {
+      missingGithubKeyword: true,
+      spaceBetweenHashtagAndDigit: true,
+    };
+    expect(violations).toEqual(expectedViolations);
   });
 });
