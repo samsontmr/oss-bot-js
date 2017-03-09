caf430ff3e984b0bdfecd29ce8612e679f80fd31
Li Kai <li-kai@users.noreply.github.com>
2017-03-09 15:32:22 +0800

    [#23] Migrate test files to es6 (#24)

--- spec/keyword_checker_spec.js
+++ spec/keyword_checker_spec.js
@@ -1,27 +1,28 @@
-var keyword_checker = require('../keyword_checker.js');
+// eslint-disable-next-line camelcase
+const keyword_checker = require('../keyword_checker.js');
 
-describe('keyword_checker', function () {
-  it('should feedback when string contains space between # and digit', function() {
-    var feedback = keyword_checker.getFeedback('Fixed # 27');
-    var expectedMessage = '   * There should not be a space between the `#` and `issue-number`.\n';
+describe('keyword_checker', () => {
+  it('should feedback when string contains space between # and digit', () => {
+    const feedback = keyword_checker.getFeedback('Fixed # 27');
+    const expectedMessage = '   * There should not be a space between the `#` and `issue-number`.\n';
     expect(feedback).toEqual(expectedMessage);
   });
 
-  it('should feedback when string is missing issue reference', function() {
-    var feedback = keyword_checker.getFeedback('Fixed something');
-    var expectedMessage = '   * Issue Reference (`#<issue-number>`) missing.\n';
+  it('should feedback when string is missing issue reference', () => {
+    const feedback = keyword_checker.getFeedback('Fixed something');
+    const expectedMessage = '   * Issue Reference (`#<issue-number>`) missing.\n';
     expect(feedback).toEqual(expectedMessage);
   });
 
-  it('should feedback when string does not contain GitHub keyword', function() {
-    var feedback = keyword_checker.getFeedback('Repaired #123');
-    var expectedMessage = '   * GitHub Keyword missing: Refer [here](https://help.github.com/articles/closing-issues-via-commit-messages/#keywords-for-closing-issues) for a list of accepted keywords.\n';
+  it('should feedback when string does not contain GitHub keyword', () => {
+    const feedback = keyword_checker.getFeedback('Repaired #123');
+    const expectedMessage = '   * GitHub Keyword missing: Refer [here](https://help.github.com/articles/closing-issues-via-commit-messages/#keywords-for-closing-issues) for a list of accepted keywords.\n';
     expect(feedback).toEqual(expectedMessage);
   });
 
-  it('should feedback both issue and keyword issues in string', function() {
-    var feedback = keyword_checker.getFeedback('Repaired # 1');
-    var expectedMessage = '   * There should not be a space between the `#` and `issue-number`.\n';
+  it('should feedback both issue and keyword issues in string', () => {
+    const feedback = keyword_checker.getFeedback('Repaired # 1');
+    let expectedMessage = '   * There should not be a space between the `#` and `issue-number`.\n';
     expectedMessage += '   * GitHub Keyword missing: Refer [here](https://help.github.com/articles/closing-issues-via-commit-messages/#keywords-for-closing-issues) for a list of accepted keywords.\n';
     expect(feedback).toEqual(expectedMessage);
   });
