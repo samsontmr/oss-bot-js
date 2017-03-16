010c5bd47c4e519921a6f66a4d6aff41400979f4
Samson <samsontmr@users.noreply.github.com>
2017-03-16 12:35:43 +0800

    Change log level to info

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const gh = new GitHub({
 app.use(bodyParser.json());
 
 function isPullRequest(receivedJson) {
-  winston.log(`Pull Request field: {${receivedJson.body.pull_request}}`);
+  winston.info(`Pull Request field: {${receivedJson.body.pull_request}}`);
   return !!receivedJson.body.pull_request;
 }
 
@@ -25,9 +25,9 @@ function extractRelevantDetails(receivedJson) {
   const title = pullRequest.title;
   const body = pullRequest.body;
   const repo = pullRequest.base.repo.full_name;
-  const username = pullRequest.user.login;
+  const username = pullRequest.user.infoin;
   const id = pullRequest.number;
-  winston.log(`Received PR ${id} "${title}" from: ${username}\nDescription: "${body}"`);
+  winston.info(`Received PR ${id} "${title}" from: ${username}\nDescription: "${body}"`);
   return {
     repo,
     id,
@@ -44,13 +44,13 @@ function isPullRequestToCheck(prDetails) {
 }
 
 function isValidPullRequestTitle(prTitle) {
-  winston.log(`Title being validated: ${prTitle}`);
-  winston.log(`Regex for title: ${process.env.REGEX_PULL_REQ_TITLE}`);
+  winston.info(`Title being validated: ${prTitle}`);
+  winston.info(`Regex for title: ${process.env.REGEX_PULL_REQ_TITLE}`);
   return utils.testRegexp(process.env.REGEX_PULL_REQ_TITLE, prTitle);
 }
 
 function isValidPullRequestBody(prBody) {
-  winston.log(`Regex for body: ${process.env.REGEX_PULL_REQ_BODY}`);
+  winston.info(`Regex for body: ${process.env.REGEX_PULL_REQ_BODY}`);
   return utils.testRegexp(process.env.REGEX_PULL_REQ_BODY, prBody);
 }
 
@@ -84,17 +84,17 @@ function getViolations(prDetails) {
 }
 
 function receivePullRequest(request, response) {
-  winston.log(`Received pull request: \n${request.body}`);
+  winston.info(`Received pull request: \n${request.body}`);
   response.send();
   if (!isPullRequest(request)) return;
   const extractedPrDetails = extractRelevantDetails(request);
   if (isPullRequestToCheck(extractedPrDetails) && !isValidPullRequest(extractedPrDetails)) {
-    winston.log('Check Failed!');
+    winston.info('Check Failed!');
     const responseMessage = messageBuilder.getFeedbackMessage(
       extractedPrDetails.username,
       getViolations(extractedPrDetails));
     commentOnPullRequest(extractedPrDetails.repo, extractedPrDetails.id, responseMessage);
-    winston.log(`Message to user: \n"${responseMessage}"`);
+    winston.info(`Message to user: \n"${responseMessage}"`);
   }
 }
 
@@ -103,7 +103,7 @@ app.post('/pull_req', receivePullRequest);
 const port = process.env.PORT || 5000;
 app.set('port', port);
 app.listen(port, () => {
-  winston.log(`Node app is running on port ${port}`);
+  winston.info(`Node app is running on port ${port}`);
 });
 
 // For unit testing purposes
